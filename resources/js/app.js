6b80c1437887802613d98ad68fa9ebb728d14f98
ghaith <a89ghaith@gmail.com>
2022-05-30 13:07:25 +0300

    move script to all.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,43 +13,67 @@ $('.select2').select2({
     debug: true
 })
 
-// $('#target_type').change(function (e) {
-//     $('.loading').removeClass('d-none')
-//     switch ($(this).val()) {
-//         case "Broadcast":
-//             $('#target_value').html('');
-//             $('#target_value').parent().addClass('d-none')
-//             break;
-//         case "Gender":
-//             var data = [{
-//                 id: "Male",
-//                 text: "Male"
-//             },
-//             {
-//                 id: "Female",
-//                 text: "Female"
-//             },
-//             ];
-//             select2(data);
-//             break;
-//         default:
-//             $('#target_value').html('');
-//             $('#target_value').parent().removeClass('d-none')
-//             $.get("/notification/get-options", {
-//                 type: $(this).val()
-//             },
-//                 function (data) {
-//                     console.log(data);
-//                     select2(data);
-//                 });
-//     }
-// });
+$(function () {
+    var url = window.location.toString()
+    // for single sidebar menu
+    $('ul.nav-sidebar a').filter(function () {
+        return url.includes(this.href);
+    }).addClass('active');
 
-// function select2(data = []) {
-//     $('.loading').addClass('d-none')
-//     $('#target_value').select2({
-//         data: data,
-//         debug: true,
-//         closeOnSelect: false,
-//     });
-// }
+    // for sidebar menu and treeview
+    $('ul.nav-treeview a').filter(function () {
+        return url.includes(this.href);
+    }).parentsUntil(".nav-sidebar > .nav-treeview")
+        .css({
+            'display': 'block'
+        })
+        .addClass('menu-open').prev('a')
+        .addClass('active');
+});
+
+targetTypeChange.apply($('#target_type'));
+
+$('#target_type').change(targetTypeChange);
+
+function targetTypeChange() {
+    $('.loading').removeClass('d-none')
+    switch ($(this).val()) {
+        case "Broadcast":
+            $('#target_value').html('');
+            $('#target_value').parent().addClass('d-none')
+            break;
+        case "Gender":
+            $('#target_value').html('');
+            $('#target_value').parent().removeClass('d-none')
+            var data = [{
+                id: "Male",
+                text: "Male"
+            },
+            {
+                id: "Female",
+                text: "Female"
+            },
+            ];
+            select2(data);
+            break;
+        default:
+            $('#target_value').html('');
+            $('#target_value').parent().removeClass('d-none')
+            $.get("/notification/get-options", {
+                type: $(this).val()
+            },
+                function (data) {
+                    console.log(data);
+                    select2(data);
+                });
+    }
+}
+
+function select2(data = []) {
+    $('.loading').addClass('d-none')
+    $('#target_value').select2({
+        data: data,
+        debug: true,
+        closeOnSelect: false,
+    });
+}
